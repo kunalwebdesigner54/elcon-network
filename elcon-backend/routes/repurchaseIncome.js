5e00f7841872d769ce345736df684b40ceb18fb6
kunalwebdesigner54 <kunalwebdesigner54@gmail.com>
2026-08-26 13:03:45 +0530

    fix: Correct auth middleware import path

--- elcon-backend/routes/repurchaseIncome.js
+++ elcon-backend/routes/repurchaseIncome.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getRepurchaseIncomeReports } = require('../controllers/repurchaseIncomeController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 router.use(protect);
 
