a62c308ff8178aa7764d660be7352ce710152cd4
Kilo <kilo@kilo.ai>
2026-09-17 17:58:45 +0530

    Fix products alignment with title and force slider arrows to show by reducing slidesToShow to 3

--- elcon-frontend/src/Components/Public/Products/PublicProducts.jsx
+++ elcon-frontend/src/Components/Public/Products/PublicProducts.jsx
@@ -29,7 +29,7 @@ const sliderSettings = {
   dots: false,
   infinite: false,
   speed: 500,
-  slidesToShow: 4,
+  slidesToShow: 3,
   slidesToScroll: 1,
   swipeToSlide: true,
   prevArrow: <SlickArrow direction="prev" />,
