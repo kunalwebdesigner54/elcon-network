1e8c074fb4fe6218c7973d9f77ba5fa32270ec67
Kilo <kilo@kilo.ai>
2026-09-17 18:56:47 +0530

    Format PublicProducts.jsx breakpoints to be multi-line

--- elcon-frontend/src/Components/Public/Products/PublicProducts.jsx
+++ elcon-frontend/src/Components/Public/Products/PublicProducts.jsx
@@ -36,9 +36,27 @@ const sliderSettings = {
   prevArrow: <SlickArrow direction="prev" />,
   nextArrow: <SlickArrow direction="next" />,
   responsive: [
-    { breakpoint: 1200, settings: { slidesToShow: 3, slidesToScroll: 1 } },
-    { breakpoint: 992, settings: { slidesToShow: 2, slidesToScroll: 1 } },
-    { breakpoint: 768, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    {
+      breakpoint: 1200,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
   ],
 };
 
