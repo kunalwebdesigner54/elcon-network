47bceeda10bba87308309aecebe2444de70a7bf9
Kilo <kilo@kilo.ai>
2026-09-23 22:45:06 +0530

    Adjust gradient to light green mixed with blue

--- elcon-frontend/src/Components/UserPanel/Rank/UserMyRank.jsx
+++ elcon-frontend/src/Components/UserPanel/Rank/UserMyRank.jsx
@@ -123,7 +123,7 @@ function UserMyRank() {
                 >
                   <div style={{ display: 'flex', alignItems: 'center' }}>
                     <div className="user-rank-circle" style={{
-                      width: '32px', height: '32px', borderRadius: '50%', background: 'linear-gradient(135deg, #00FF87 0%, #60EFFF 100%)', 
+                      width: '32px', height: '32px', borderRadius: '50%', background: 'linear-gradient(135deg, #2af598 0%, #009efd 100%)', 
                       display: 'flex', alignItems: 'center', justifyContent: 'center', color: '#fff', fontWeight: 'bold', marginRight: '15px'
                     }}>
                       {index + 1}
