7e753d22c2af600976bbf02cd6f4bf1950347925
Kilo <kilo@kilo.ai>
2026-09-26 23:41:24 +0530

    style: increase home banner slider speed

--- elcon-frontend/src/Components/Public/Home/Home.jsx
+++ elcon-frontend/src/Components/Public/Home/Home.jsx
@@ -36,11 +36,11 @@ function Home() {
   const sliderSettings = {
     dots: false,
     infinite: true,
-    speed: 1000,
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 4000,
+    autoplaySpeed: 2500,
     fade: true,
     arrows: true,
     waitForAnimate: false
